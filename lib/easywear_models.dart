cb1982cb675c3a02364248e6aa241871eaf5e619
unknown <felixbruebach@outlook.de>
2025-04-15 23:34:10 +0200

    fix

--- lib/easywear_models.dart
+++ lib/easywear_models.dart
@@ -26,3 +26,4 @@ export 'src/models/white_label.dart';
 export 'src/models/workwear.dart';
 export 'src/models/package_entry.dart';
 export 'src/models/package.dart';
+export 'src/models/order_prompt.dart';
