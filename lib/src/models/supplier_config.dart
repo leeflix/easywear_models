7d50869dd4f0976ffc5c97cd509ea032de01e423
unknown <felixbruebach@outlook.de>
2025-04-20 00:46:19 +0200

    fix

--- lib/src/models/supplier_config.dart
+++ lib/src/models/supplier_config.dart
@@ -2,36 +2,26 @@ import 'dart:convert';
 
 class SupplierConfig {
   Set<String> customerDomainIds;
-  Map<String, Map<String, Map<String, double?>>> shops;
+  Map<String, Set<String>> domainIdToWorkwearIds;
   String? returnPolicyUrl;
 
   SupplierConfig({
     required this.customerDomainIds,
-    required this.shops,
+    required this.domainIdToWorkwearIds,
     required this.returnPolicyUrl,
   });
 
   Map<String, dynamic> toJson() => {
         "customerDomainIds": customerDomainIds.toList(),
-        "shops": shops.map(
-          (key, value) => MapEntry(
-            key,
-            value.map(
-              (key, value) => MapEntry(
-                key,
-                value.map(
-                  (key, value) => MapEntry(key, value),
-                ),
-              ),
-            ),
-          ),
-        ),
+        "domainIdToWorkwearIds": domainIdToWorkwearIds.map((key, value) => MapEntry(key, value.toList())),
         "returnPolicyUrl": returnPolicyUrl,
       };
 
   SupplierConfig.fromJson(Map<String, dynamic> json)
       : customerDomainIds = Set<String>.from(json["customerDomainIds"]),
-        shops = Map<String, Map<String, Map<String, double?>>>.from(json["shops"]),
+        domainIdToWorkwearIds = (json["domainIdToWorkwearIds"] as Map<String, dynamic>).map(
+          (key, value) => MapEntry(key, Set<String>.from(value)),
+        ),
         returnPolicyUrl = json["returnPolicyUrl"];
 
   @override
