20b257a2712c289ce2fe71c277bd847f77843304
unknown <felixbruebach@outlook.de>
2025-04-21 17:51:00 +0200

    fix

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -14,7 +14,7 @@ class User extends Model<User> {
   String? lastName;
   String? personnelNumber;
   String? imageId;
-  String userName;
+  String? userName;
   String? companyMail;
   String? contactMail;
   String? phoneNumber;
