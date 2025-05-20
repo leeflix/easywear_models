a34d147599f5b4b3cc627f14660144e48a8c31ec
unknown <felixbruebach@outlook.de>
2025-05-21 01:30:37 +0200

    fix

--- lib/src/models/supplier_config.dart
+++ lib/src/models/supplier_config.dart
@@ -4,17 +4,24 @@ class SupplierConfig {
   Set<String> customerDomainIds;
   Map<String, Set<String>> domainIdToWorkwearIds;
   String? returnPolicyUrl;
+  String supportEmail;
+  String supportPhone;
 
   SupplierConfig({
     required this.customerDomainIds,
     required this.domainIdToWorkwearIds,
     required this.returnPolicyUrl,
+    required this.supportEmail,
+    required this.supportPhone,
   });
 
   Map<String, dynamic> toJson() => {
         "customerDomainIds": customerDomainIds.toList(),
-        "domainIdToWorkwearIds": domainIdToWorkwearIds.map((key, value) => MapEntry(key, value.toList())),
+        "domainIdToWorkwearIds": domainIdToWorkwearIds
+            .map((key, value) => MapEntry(key, value.toList())),
         "returnPolicyUrl": returnPolicyUrl,
+        "supportEmail": supportEmail,
+        "supportPhone": supportPhone,
       };
 
   SupplierConfig.fromJson(Map<String, dynamic> json)
@@ -22,7 +29,9 @@ class SupplierConfig {
         domainIdToWorkwearIds = (json["domainIdToWorkwearIds"] as Map).map(
           (key, value) => MapEntry(key, Set<String>.from(value)),
         ),
-        returnPolicyUrl = json["returnPolicyUrl"];
+        returnPolicyUrl = json["returnPolicyUrl"],
+        supportEmail = json["supportEmail"],
+        supportPhone = json["supportPhone"];
 
   @override
   String toString() => jsonEncode(this);
