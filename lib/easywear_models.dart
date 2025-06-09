06bd6b5c5e049c3083aae4130979c9f143d4e184
unknown <felixbruebach@outlook.de>
2025-06-09 23:12:11 +0200

    fix

--- lib/easywear_models.dart
+++ lib/easywear_models.dart
@@ -27,3 +27,4 @@ export 'src/models/package_entry.dart';
 export 'src/models/package.dart';
 export 'src/models/order_prompt.dart';
 export 'src/models/category.dart';
+export 'src/models/beekeeper.dart';
