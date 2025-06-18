e8851e8b82c0054747afc914299f2b21b2d197cf
unknown <felixbruebach@outlook.de>
2025-06-18 13:52:47 +0200

    fix

--- lib/src/models/order_prompt.dart
+++ lib/src/models/order_prompt.dart
@@ -2,23 +2,19 @@ import 'dart:convert';
 
 class OrderPrompt {
   String? starterKitName;
-  Map<String, int> workwearIdToAmount;
   String supplierDomainId;
 
   OrderPrompt({
     required this.starterKitName,
-    required this.workwearIdToAmount,
     required this.supplierDomainId,
   });
 
   OrderPrompt.fromJson(Map<String, dynamic> json)
       : starterKitName = json["starterKitName"],
-        workwearIdToAmount = Map<String, int>.from(json["workwearIdToAmount"]),
         supplierDomainId = json["supplierDomainId"];
 
   Map<String, dynamic> toJson() => {
         "starterKitName": starterKitName,
-        "workwearIdToAmount": workwearIdToAmount,
         "supplierDomainId": supplierDomainId,
       };
 
