abca826ce6a12ae4c29b1384bf7e75d3826ebaf4
unknown <felixbruebach@outlook.de>
2025-06-30 04:27:03 +0200

    fix

--- lib/src/models/permissions.dart
+++ lib/src/models/permissions.dart
@@ -1,29 +1,29 @@
-import 'package:easywear_models/easywear_models.dart';
+  import 'package:easywear_models/easywear_models.dart';
 
-class Permissions {
-  Set<DomainId> domainIds;
-  Set<LocationId> locationIds;
-  Set<DepartmentId> departmentIds;
+  class Permissions {
+    Set<DomainId> domainIds;
+    Set<LocationId> locationIds;
+    Set<DepartmentId> departmentIds;
 
-  Permissions({
-    required this.domainIds,
-    required this.locationIds,
-    required this.departmentIds,
-  });
+    Permissions({
+      required this.domainIds,
+      required this.locationIds,
+      required this.departmentIds,
+    });
 
-  Permissions.fromJson(Map<String, dynamic> json)
-      : domainIds = Set<String>.from(json["domainIds"]),
-        locationIds = Set<String>.from(json["locationIds"]),
-        departmentIds = Set<String>.from(json["departmentIds"]);
+    Permissions.fromJson(Map<String, dynamic> json)
+        : domainIds = Set<String>.from(json["domainIds"]),
+          locationIds = Set<String>.from(json["locationIds"]),
+          departmentIds = Set<String>.from(json["departmentIds"]);
 
-  Map<String, dynamic> toJson() => {
-        "domainIds": domainIds.toList(),
-        "locationIds": locationIds.toList(),
-        "departmentIds": departmentIds.toList(),
-      };
+    Map<String, dynamic> toJson() => {
+          "domainIds": domainIds.toList(),
+          "locationIds": locationIds.toList(),
+          "departmentIds": departmentIds.toList(),
+        };
 
-  Permissions.none()
-      : domainIds = {},
-        locationIds = {},
-        departmentIds = {};
-}
+    Permissions.none()
+        : domainIds = {},
+          locationIds = {},
+          departmentIds = {};
+  }
