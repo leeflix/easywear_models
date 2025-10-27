64d04fddafb17cdec058d7135874e6545b35feba
Felix <felixbruebach@outlook.de>
2025-10-27 10:49:04 +0100

    Add order delivery confirmation feature
    - Add awaitingUserConfirmation status to RequestStatus enum with 28 language translations
    - Add requireUserOrderConfirmation flag to Features model (defaults to false)

--- lib/src/models/features.dart
+++ lib/src/models/features.dart
@@ -1,18 +1,22 @@
 class Features {
   bool deposit;
   bool correction;
+  bool requireUserOrderConfirmation;
 
   Features({
     required this.deposit,
     required this.correction,
+    required this.requireUserOrderConfirmation,
   });
 
   Features.fromJson(Map<String, dynamic> json)
       : deposit = json["deposit"],
-        correction = json["correction"];
+        correction = json["correction"],
+        requireUserOrderConfirmation = json["requireUserOrderConfirmation"] ?? false;
 
   Map<String, dynamic> toJson() => {
         "deposit": deposit,
         "correction": correction,
+        "requireUserOrderConfirmation": requireUserOrderConfirmation,
       };
 }
